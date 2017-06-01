0ec994d17103074421eab6ccbac96499f4717d6c
Samuel Brass <Sam@Samuels-MacBook-Pro.local>
2017-06-01 18:49:31 +1000

    Initial Structure

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,19 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+/* SAM SECTION. */
+
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'The Annual Chip' });
 });
+
+/* TOM SECTION. */
+
+router.get('/scorecard', function(req, res, next) {
+  res.render('scorecard', { title: 'Scorecard' });
+});
+
+/* General Stuff */
 
 module.exports = router;
 
