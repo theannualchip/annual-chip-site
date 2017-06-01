55acf664a007cf53f177f85d312fd41850a00569
Samuel Brass <Sam@Samuels-MacBook-Pro.local>
2017-06-01 21:46:26 +1000

    Server DB Connection Example

--- routes/index.js
+++ routes/index.js
@@ -1,20 +1,36 @@
+/* Dependancies */
+
 var express = require('express');
 var router = express.Router();
+var pgp = require('pg-promise')();
+
+/* DB Connection */
+
+const db_connection_string = 'https://the_annual_chip:the_annual_chip@chip-db.cclyf5gm9q8m.us-west-2.rds.amazonaws.com:5432/the_annual_chip';
+var db = pgp(db_connection_string);
 
 /* SAM SECTION. */
 
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'The Annual Chip' });
+    db.query('SELECT * FROM users_example WHERE username = $1', ['T Bish'])
+        .then(data => {
+            console.log(JSON.stringify(data));
+            res.render('index', {
+                title: 'The Annual Chip',
+                users: JSON.stringify(data)
+            });
+        })
+        .catch(error => {
+            console.log(error);
+        });
 });
 
 /* TOM SECTION. */
 
 router.get('/scorecard', function(req, res, next) {
-  res.render('scorecard', { title: 'Scorecard' });
+    res.render('scorecard', { title: 'Scorecard' });
 });
 
 /* General Stuff */
 
 module.exports = router;
-
-
