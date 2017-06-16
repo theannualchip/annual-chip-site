f974b8c525b593ec5ad86d90252d840655bd6cb8
Samuel Brass <Sam@Samuels-MacBook-Pro.local>
2017-06-17 08:18:59 +1000

    db refactor

--- routes/index.js
+++ routes/index.js
@@ -4,26 +4,27 @@ var express = require('express');
 var router = express.Router();
 var pgp = require('pg-promise')();
 var tom_js = require("../public/javascripts/tom.js");
+var db= require("../db.js");
 
 /* DB Connection */
 
-const db_connection_string = 'https://the_annual_chip:the_annual_chip@chip-db.cclyf5gm9q8m.us-west-2.rds.amazonaws.com:5432/the_annual_chip';
+//const db_connection_string = 'https://the_annual_chip:the_annual_chip@chip-db.cclyf5gm9q8m.us-west-2.rds.amazonaws.com:5432/the_annual_chip';
 //var db = pgp(db_connection_string);
 
 /* SAM SECTION. */
 
 router.get('/', function(req, res, next) {
-    /*db.query('SELECT * FROM users_example WHERE username = $1', ['T Bish'])
+    db.query('SELECT * FROM users_example WHERE username = $1', ['T Bish'])
         .then(data => {
             console.log(JSON.stringify(data));
-            res.render('index', {
+            /*res.render('index', {
                 title: 'The Annual Chip',
                 users: JSON.stringify(data)
-            });
+            });*/
         })
         .catch(error => {
             console.log(error);
-        });*/
+        });
         res.render('index',{title: 'The Annual Chip',
                 users: "Whatever"
             });
