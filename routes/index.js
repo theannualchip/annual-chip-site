6c69a483214234bcb40165f7fe6789cd1d74f371
Samuel Brass <Sam@Samuels-MacBook-Pro.local>
2017-06-17 13:09:53 +1000

    new login

--- routes/index.js
+++ routes/index.js
@@ -6,28 +6,14 @@ var pgp = require('pg-promise')();
 var tom_js = require("../public/javascripts/tom.js");
 var db= require("../db.js");
 
-/* DB Connection */
-
-//const db_connection_string = 'https://the_annual_chip:the_annual_chip@chip-db.cclyf5gm9q8m.us-west-2.rds.amazonaws.com:5432/the_annual_chip';
-//var db = pgp(db_connection_string);
-
 /* SAM SECTION. */
 
 router.get('/', function(req, res, next) {
-    db.query('SELECT * FROM users_example WHERE username = $1', ['T Bish'])
-        .then(data => {
-            console.log(JSON.stringify(data));
-            /*res.render('index', {
-                title: 'The Annual Chip',
-                users: JSON.stringify(data)
-            });*/
-        })
-        .catch(error => {
-            console.log(error);
-        });
-        res.render('index',{title: 'The Annual Chip',
-                users: "Whatever"
-            });
+    res.redirect('/login');
+});
+
+router.get('/login', function(req, res, next) {
+    res.render('login');
 });
 
 /* TOM SECTION. */
