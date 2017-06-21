7e97b641baf635f5ab5acdf335b419049e84abce
Tom Bishop <tomwbish@gmail.com>
2017-06-21 20:24:25 +1000

    DB changes, add to notes, dynamic scoreboard udated, cosmetics

--- public/javascripts/tom.js
+++ public/javascripts/tom.js
@@ -1,8 +1,46 @@
+var multiline = require('multiline');
+
 exports.get_short_lboard = function(db_connection, day) {
 
 	db = db_connection;
 
-	result = db.query('SELECT username, sum(score) as score FROM scores WHERE day = $1 GROUP BY username ORDER BY score', [day])
+	query_string =  multiline.stripIndent(function(){/*
+		
+		WITH user_info AS (
+			SELECT
+				a.username,
+				b.photo_title
+			FROM
+				golfers a
+				INNER JOIN photos b ON (
+					a.username = b.uploaded_user)
+		),
+
+		scores as (
+			SELECT
+				username,
+				sum(score) as score
+			FROM
+				scores
+			WHERE
+				day = $1
+			GROUP BY
+				username
+		)	
+
+		SELECT
+			a.*,
+			COALESCE(b.score, 99) as score
+		FROM
+			user_info a
+			LEFT JOIN scores b ON (
+				a.username = b.username)
+		ORDER BY
+			score;
+	*/
+	});
+
+	result = db.query(query_string, [day])
 
 	return result;
 
@@ -16,27 +54,24 @@ exports.format_lboard = function(lboard) {
 
 		var name = lboard[i].username;
 		var score = lboard[i].score;
-		var img = "";
+		var img = lboard[i].photo_title;
 
-		table += "<div>"
-		
-		switch(lboard[i].username) {
-		    case "T Bish":
-		        img = "tom.jpg";
-		        break;
-		    case "Sammy B":
-		        img = "sam.jpg";
-		        break;
-		    };
+		// table += "<div class='columns'>"
+		// table += '<div class="column is-4 align-middle-col "><img class="img-circle" src="/img/' + img + '"></div>'
+		// table += '<div class="column is-4 align-middle-col "><span class="is-medium has-text-centered align-middle-el">' + name + '</span></div>'  
+		// table += '<div class="column is-4 align-middle-col "><span class="is-medium has-text-centered align-middle-el">' + score + '</span></div></div>'  
 
-		table += '<img class="img-circle" src="/img/' + img + '">'
-		table +=  '<h6 class="float-left">' + name + '</h6></div>'  
+		table += "<div class='columns'>"
+		table += '<div class="column is-4 center-div"><img class="img-circle" src="/img/' + img + '"></div>'
+		table += '<div class="column is-4 center-div"><strong>' + name + '</strong></div>'  
+		table += '<div class="column is-4 center-div"><strong>' + score + '</strong></div></div>'  
 
 	}
 
 	return table;
 
 };
+
 
 function my_function() {
 
