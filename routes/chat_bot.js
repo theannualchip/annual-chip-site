7851e508d9b8ab2bb47f74bc9d543d4467efca81
Samuel Brass <Sam@Samuels-MacBook-Pro.local>
2017-06-27 22:07:20 +1000

    some chat functionality

--- routes/chat_bot.js
+++ routes/chat_bot.js
@@ -1,4 +1,14 @@
+var db = require("../db.js");
+var moment = require('moment');
 
 module.exports = function(req, message) {
-    req.app.io.emit('chat message', { user: 'Chat Bot', message: message });
+    db.query("INSERT INTO chat (user_email,comment,timestamp) VALUES ($1,$2,$3)", ['chat_bot', message, moment()])
+        .then(function(data) {
+            console.log("\x1b[42m\x1b[37mSuccessfully added \x1b[0m \x1b[34m" + new_file_name + "\x1b[0m to photos db");
+            req.app.io.emit('chat message', { username: "Chat Bot", message: message, profile_photo_title: 'chat_bot.jpg', timestamp: moment() });
+        })
+        .catch(function(error) {
+            console.log("Couldn't upload chat bot message \x1b[34m" + message + "\x1b[31m error quering the chat database\x1b[0m:");
+            console.log(error);
+        });
 }
--- routes/sockets.js
+++ routes/sockets.js
@@ -24,10 +24,11 @@ module.exports = function(io) {
                         console.log("\x1b[42m\x1b[37mSuccessfully connected socket for\x1b[0m \x1b[34m" + user_email + "\x1b[0m");
                         socket.useremail = user_email;
                         socket.username = data[0].username;
+                        socket.profile_photo_title = data[0].profile_photo_title;
                         //check who is online emmit online object
                         online_users = get_online_users(io.sockets.connected);
                         io.emit('online update', online_users);
-                        io.emit('chat message', { user: "Chat Bot", message: socket.username + " just joined the conversation." });
+                        io.emit('chat message', { username: "Chat Bot", message: socket.username + " just joined the conversation.", profile_photo_title: 'chat_bot.jpg', timestamp: moment() });
                     } else {
                         console.log("Couldn't connect socket for \x1b[34m" + user_email + "\x1b[31m the email they provided is not valid\x1b[0m:");
                         socket.disconnect();
@@ -42,16 +43,22 @@ module.exports = function(io) {
         socket.on('disconnect', function() {
             if (socket.useremail) {
                 console.log("\x1b[42m\x1b[37mSuccessfully disconnected socket for\x1b[0m \x1b[34m" + socket.useremail + "\x1b[0m");
-                io.emit('chat message', { user: "Chat Bot", message: socket.username + " has left the conversation." });
+                io.emit('chat message', { username: "Chat Bot", message: socket.username + " has left the conversation.", profile_photo_title: 'chat_bot.jpg', timestamp: moment() });
             } else {
                 console.log("\x1b[31m Unusual disconnect for \x1b[0m" + socket.id + "\x1b[31m. No username or email included in socket object \x1b[0m");
             }
         });
 
-        socket.on('chat message', function(msg) {
-            io.emit('chat message', msg);
+        socket.on('chat message', function(message) {
+            db.query("INSERT INTO chat (user_email,comment,timestamp) VALUES ($1,$2,$3)", [socket.useremail, message, moment()])
+                .then(function(data) {
+                    console.log("\x1b[42m\x1b[37mSuccessfully added \x1b[0m \x1b[34m" + message + "\x1b[0m to chat db");
+                    io.emit('chat message', { username: socket.username, message: message, profile_photo_title: socket.profile_photo_title, timestamp: moment() });
+                })
+                .catch(function(error) {
+                    console.log("Couldn't upload message \x1b[34m" + message + "\x1b[31m error quering the chat database\x1b[0m:");
+                    console.log(error);
+                });
         });
-
-
     });
 }
